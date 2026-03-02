2119d9330adf909a7b70cebbbc888ec91a978544
gusttavoteles <gusttavogomes09@gmail.com>
2026-03-02 12:56:37 -0300

    Update login.js

--- projeto/login.js
+++ projeto/login.js
@@ -4,7 +4,7 @@ function validarLogin() {
 
     if (usuario === "adm@1234" && senha === "1234") {
         alert("Login realizado com sucesso!");
-        window.location.href = "/projeto/html/adm.html";
+        window.location.href = "/system_mobile1/projeto/html/adm.html"";
         return false;
     } else {
         alert("Usuário ou senha incorretos!");
@@ -12,4 +12,5 @@ function validarLogin() {
     }
 
 }
+
 
